3b06d1381a28d0396d29f90a7f367b63cf00ec52
dave-hickman <dave.robert.hickman@gmail.com>
2023-06-26 10:36:03 +0100

    add project cards to project page

--- src/Pages/ProjectsPage.tsx
+++ src/Pages/ProjectsPage.tsx
@@ -1,4 +1,6 @@
 import SingleProject from "../Components/SingleProject";
+import "../Styling/ProjectsPage.css"
+import "../Styling/Projects.css"
 
 interface Project {
     image: string;
@@ -13,13 +15,15 @@ interface Project {
 
 const ProjectsPage = ({currentProjects}:ProjectsPageProps ) => {
     return (
-        <>
-        <h1>Projects</h1>
+        <div className="projects-page">
+        <h1 className="projects-page-title">Projects</h1>
+        <section className="projects-page-cards">
         {currentProjects.map((project)=> {
             return(
                  <SingleProject key={project.name} project={project}/>)
         })}
-        </>
+        </section>
+        </div>
     )
 }
 
